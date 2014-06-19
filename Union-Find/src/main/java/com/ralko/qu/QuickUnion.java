636116e8d29c56e85f970d84b923f4740f0475af
Qualitative <qualitative1@gmail.com>
2014-06-19 23:49:54 +0300

    Added implementation of QuickUnion

--- Union-Find/src/main/java/com/ralko/qu/QuickUnion.java
+++ Union-Find/src/main/java/com/ralko/qu/QuickUnion.java
@@ -1,15 +1,16 @@
-package com.ralko.qf;
+package com.ralko.qu;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
 import java.util.Arrays;
+
 import org.apache.commons.lang3.StringUtils;
 
-public class QuickFind {
+public class QuickUnion {
 
     private final int[] items;
 
-    public QuickFind(int initialCapacity) {
+    public QuickUnion(int initialCapacity) {
         checkArgument(initialCapacity > 0, "Capacity should be positive: %s", initialCapacity);
         items = new int[initialCapacity];
 
@@ -19,25 +20,25 @@ public class QuickFind {
     }
 
     public void union(int p, int q) {
-        int pid = items[p];
-        int qid = items[q];
-
-        if (pid == qid) return;
-
-        for(int i = 0; i < items.length; i++) {
-            if (items[i] == pid) items[i] = qid;
-        }
-
+        int proot = root(p);
+        int qroot = root(q);
+        items[qroot] = proot;
     }
 
+
     public boolean find(int p, int q) {
-        return items[p] == items[q];
+        return root(p) == root(q);
     }
 
     public String getState() {
         String itemsAsString = Arrays.toString(items);
         itemsAsString = StringUtils.remove(itemsAsString, ',');
         return itemsAsString;
+    }
+
+    private int root(int i) {
+        while(i != items[i]) i = items[i];
+        return i;
     }
 
 }
---
+++ Union-Find/src/test/java/com/ralko/qu/QuickUnionTest.java
@@ -0,0 +1,65 @@
+package com.ralko.qu;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.ralko.qf.QuickFind;
+
+public class QuickUnionTest {
+
+    private static final int INITIAL_CAPACITY = 10;
+    private QuickUnion qu;
+
+    @Before
+    public void init() {
+        qu = new QuickUnion(INITIAL_CAPACITY);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhenInitialCapacityIsNotPositive() {
+        new QuickFind(0);
+    }
+
+    @Test
+    public void shouldInitializeAllItemsWithDefaultValuesWhenCreated() {
+        assertEquals("[0 1 2 3 4 5 6 7 8 9]", qu.getState());
+    }
+
+    @Test
+    public void shouldChangeSetForFirstArgumentWhenUnionPair() {
+        qu.union(0, 1);
+        assertEquals("[0 0 2 3 4 5 6 7 8 9]", qu.getState());
+        qu.union(9, 8);
+        assertEquals("[0 0 2 3 4 5 6 7 9 9]", qu.getState());
+        qu.union(8, 1);
+        assertEquals("[9 0 2 3 4 5 6 7 9 9]", qu.getState());
+    }
+
+    @Test
+    public void shouldReturnFalseWhenItemsAreNotConnected() {
+        assertFalse(qu.find(0, 1));
+        assertFalse(qu.find(9, 8));
+        assertFalse(qu.find(8, 1));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenItemsAreConnected() {
+        qu.union(0, 1);
+        qu.union(9, 8);
+        qu.union(8, 1);
+
+        assertTrue(qu.find(0, 1));
+        assertTrue(qu.find(9, 8));
+        assertTrue(qu.find(8, 1));
+        assertTrue(qu.find(1, 0));
+        assertTrue(qu.find(8, 9));
+        assertTrue(qu.find(1, 8));
+        assertTrue(qu.find(0, 9));
+        assertTrue(qu.find(0, 8));
+        assertTrue(qu.find(1, 9));
+    }
+}
