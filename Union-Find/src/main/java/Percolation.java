fb01befcb75bb9531a59d4c5a5100abeb50a499b
Qualitative <qualitative1@gmail.com>
2014-06-20 22:08:28 +0300

    Prepared stubs for assignment classes

--- Union-Find/src/main/java/Percolation.java
+++ Union-Find/src/main/java/Percolation.java
@@ -1,7 +1,15 @@
 
 
+/**
+ * @author Qualitative
+ *
+ * The indices i and j are integers between 1 and N, where (1, 1) is the upper-left site.
+ * Throw a java.lang.IndexOutOfBoundsException if either i or j is outside this range.
+ * The constructor should take time proportional to N^2.
+ * All methods should take constant time plus a constant number of calls to the union-find methods union(), find(), connected(), and count().
+ */
 public class Percolation {
-    
+
     public Percolation(int N) {             // create N-by-N grid, with all sites blocked
         // TODO Auto-generated constructor stub
     }
@@ -9,7 +17,7 @@ public class Percolation {
     public void open(int i, int j) {        // open site (row i, column j) if it is not already
         // TODO Auto-generated method stub
     }
-    
+
     public boolean isOpen(int i, int j) {   // is site (row i, column j) open?
         // TODO Auto-generated method stub
         return false;
---
+++ Union-Find/src/main/java/PercolationStats.java
@@ -0,0 +1,32 @@
+
+public class PercolationStats {
+
+   public PercolationStats(int N, int T) {   // perform T independent computational experiments on an N-by-N grid
+       // TODO: provide implementation
+   }
+
+   public double mean() {                    // sample mean of percolation threshold
+       // TODO: provide implementation
+       return Double.NaN;
+   }
+
+   public double stddev() {                  // sample standard deviation of percolation threshold
+       // TODO: provide implementation
+       return Double.NaN;
+   }
+
+   public double confidenceLo() {            // returns lower bound of the 95% confidence interval
+       // TODO: provide implementation
+       return Double.NaN;
+   }
+
+   public double confidenceHi() {            // returns upper bound of the 95% confidence interval
+       // TODO: provide implementation
+       return Double.NaN;
+   }
+
+   public static void main(String[] args) {  // test client, described below
+
+   }
+
+}
