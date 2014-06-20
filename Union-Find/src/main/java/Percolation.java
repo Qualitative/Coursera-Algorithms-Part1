e68b686fd5ed23da75b3a71290a4afaa21c4f897
Qualitative <qualitative1@gmail.com>
2014-06-20 18:23:03 +0300

    Added resources for testing Percolation

---
+++ Union-Find/src/main/java/Percolation.java
@@ -0,0 +1,28 @@
+
+
+public class Percolation {
+    
+    public Percolation(int N) {             // create N-by-N grid, with all sites blocked
+        // TODO Auto-generated constructor stub
+    }
+
+    public void open(int i, int j) {        // open site (row i, column j) if it is not already
+        // TODO Auto-generated method stub
+    }
+    
+    public boolean isOpen(int i, int j) {   // is site (row i, column j) open?
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean isFull(int i, int j) {   // is site (row i, column j) full?
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean percolates() {           // does the system percolate?
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
